fbf257ceedaec3a64ee3d91f375a5ef28fc904e3
HarryDang <danghuuphuc001@gmail.com>
2024-11-29 16:43:01 +0700

    feat(logger && validation): logger with nestjs-pino pino-http && validation class-validator

--- apps/reservations/src/dto/create-reservation.dto.ts
+++ apps/reservations/src/dto/create-reservation.dto.ts
@@ -1,4 +1,13 @@
+import { IsDate, IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateReservationDto {
+  // @IsDate()
+
+  @IsNotEmpty()
+  @IsString()
   placeId: string;
+
+  @IsNotEmpty()
+  @IsString()
   invoiceId: string;
 }
--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { DatabaseModule } from '@app/common';
+import { DatabaseModule, LoggerModule } from '@app/common';
 
 import { ReservationsService } from './reservations.service';
 import { ReservationsController } from './reservations.controller';
@@ -15,6 +15,7 @@ import {
     DatabaseModule.forFeature([
       { name: ReservationDocument.name, schema: ReservationSchema },
     ]),
+    LoggerModule,
   ],
   controllers: [ReservationsController],
   providers: [ReservationsService, ReservationsRepository],
