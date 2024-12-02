7a15920d6b836be0d2324e4ede75c025fcdff211
HarryDang <danghuuphuc001@gmail.com>
2024-12-02 21:43:22 +0700

    fix(auth): fix create user can't be called in postman

--- apps/auth/src/users/users.module.ts
+++ apps/auth/src/users/users.module.ts
@@ -2,17 +2,17 @@ import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { DatabaseModule } from '@app/common';
-import { UserDocument } from './models/user.schema';
+import { UserDocument, UserSchema } from './models/user.schema';
 import { UsersRepository } from './user.repository';
 
 @Module({
-  controllers: [UsersController],
-  providers: [UsersService, UsersRepository],
   imports: [
     DatabaseModule,
     DatabaseModule.forFeature([
-      { name: UserDocument.name, schema: UserDocument },
+      { name: UserDocument.name, schema: UserSchema },
     ]),
   ],
+  controllers: [UsersController],
+  providers: [UsersService, UsersRepository],
 })
 export class UsersModule {}
