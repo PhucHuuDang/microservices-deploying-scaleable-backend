0047b4d16d3f0194a9a21800416156293b3ba181
HarryDang <danghuuphuc001@gmail.com>
2024-12-04 16:45:13 +0700

    feat(passport-local): implement authentication with jwt

--- apps/auth/src/users/users.service.ts
+++ apps/auth/src/users/users.service.ts
@@ -1,11 +1,29 @@
 import { UsersRepository } from './user.repository';
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly usersRepository: UsersRepository) {}
   async createUser(createUserDto: CreateUserDto) {
-    return await this.usersRepository.create(createUserDto);
+    const { email, password } = createUserDto;
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    return await this.usersRepository.create({
+      email,
+      password: hashedPassword,
+    });
+  }
+
+  async verifyUser(email: string, password: string) {
+    const user = await this.usersRepository.findOne({ email });
+    const passwordIsValid = await bcrypt.compare(password, user.password);
+
+    if (!passwordIsValid) {
+      throw new UnauthorizedException('Credentials are not valid.');
+    }
+    return user;
   }
 }
